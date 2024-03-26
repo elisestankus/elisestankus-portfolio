d4de4b2754730340095c39fcf3cdce755234048e
estan <elise.stankus@gmail.com>
2024-03-26 11:16:40 -0700

    imported pages, set up router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import AboutMe from './pages/AboutMe.jsx';
+import Work from './pages/Work.jsx';
+
+const router = createBrowserRouter([
+  {
+      path: '/',
+      element: <App />,
+      children: [
+          {
+              index: true,
+              element: <Home />,
+          },
+          {
+              path: '/AboutMe',
+              element: <AboutMe />,
+          },
+          {
+              path: '/Work',
+              element: <Work />,
+          },
+      ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
