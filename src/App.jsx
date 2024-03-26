da3ac255ec2c9aed57427bd58297367d18d58b90
estan <elise.stankus@gmail.com>
2024-03-26 15:15:43 -0700

    added margins to box

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Nav />
-      <Box component="main" sx={{ m: 5, p: 3 }}>
+      <Box component="main" sx={{ my: 10, mx: 5, p: 3 }}>
         <Outlet />
       </Box>
       {/* <Footer /> */}
