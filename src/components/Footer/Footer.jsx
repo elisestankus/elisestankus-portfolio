81cbf17c2d26983ae216b7af07d1e0c61fc4f251
estan <elise.stankus@gmail.com>
2024-03-27 15:06:39 -0700

    created footer, added github and linkedin icons, linked profiles

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+function Footer(){
+    return (
+        <footer className='footer'>
+            <a href="https://github.com/elisestankus"><GitHubIcon /></a> <a href="https://linkedin.com/in/elise-stankus-1679b7140"><LinkedInIcon /></a>
+        </footer>
+    )
+}
+
+export default Footer;
+
+
