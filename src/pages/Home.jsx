312942cdd2a8a02b081d0e5b3db630e1154cb0a6
estan <elise.stankus@gmail.com>
2024-03-27 14:24:41 -0700

    added interactivity

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from "@mui/material/Box";
 import  Typography  from "@mui/material/Typography";
 import  TextField  from "@mui/material/TextField";
-import { grey } from '@mui/material/colors';
+
 
 const theme = createTheme({
     palette: {
@@ -16,16 +17,44 @@ const theme = createTheme({
 
 export default function Home() {
 
+    const [userName, setUserName] = useState('');
+    const [comma, setComma] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+
+        if (value.length > 0) {
+            setComma(', ');
+        } else {
+            setComma('')
+        }
+
+        return setUserName(value);
+
+    };
+
     
+
+
     
     return (
         <div>
             <Box alignItems="center">
-            <Typography variant="h1" sx={{fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif'}}>Welcome!</Typography>
+            <Typography variant="h1" sx={{fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif'}}>Welcome{comma}{userName}!</Typography>
             <Typography variant="h1" sx={{fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif'}}>I'm Elise Stankus,</Typography>
             <Typography variant="h2" sx={{fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif'}}>a full-stack web developer.</Typography>
             <ThemeProvider theme={theme}>
-            <TextField label="Enter your name!" size='small' variant="outlined" color='primary' InputLabelProps={{sx: {fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif'}}} sx={{my: 3, input: {color: 'whitesmoke'}}} InputProps={{sx: {fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif', }}}focused />
+            <TextField 
+            label="Enter your name!"
+            value={userName} 
+            size='small' 
+            variant="outlined" 
+            color='primary' 
+            InputLabelProps={{sx: {fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif'}}} 
+            sx={{my: 3, input: {color: 'whitesmoke'}}} 
+            InputProps={{sx: {fontFamily: '"Georgia", "Helvetica", "Arial", sans-serif', }}}
+            focused 
+            onChange={handleInputChange}/>
             </ThemeProvider>
             </Box>
             
