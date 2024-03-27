3ecd659cbd612d43a1a2e764c8d46dc328c798da
estan <elise.stankus@gmail.com>
2024-03-27 15:35:02 -0700

    styled footer icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,10 +2,14 @@ import * as React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
+import './Footer.css'
+
 function Footer(){
     return (
         <footer className='footer'>
-            <a href="https://github.com/elisestankus"><GitHubIcon /></a> <a href="https://linkedin.com/in/elise-stankus-1679b7140"><LinkedInIcon /></a>
+            <div>
+            <a href="https://github.com/elisestankus"><GitHubIcon fontSize='large' sx={{mx: 3, my: 1}}/></a> <a href="https://linkedin.com/in/elise-stankus-1679b7140"><LinkedInIcon fontSize='large' sx={{mx: 3, my: 1}}/></a>
+            </div>
         </footer>
     )
 }
