a42858621ecfce0f3519d0adf4a09bd9c9e2fe34
estan <elise.stankus@gmail.com>
2024-04-01 16:06:15 -0700

    add cc screenshot to project card

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,15 +1,17 @@
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import ccScreenshot from '../../assets/ccScreenshot.png';
 
 function ProjectCard() {
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <Paper> 
+            <Paper>
+                <img src={ccScreenshot} alt="project screenshot" height="200px" width="200px" /> 
                 <Typography variant="h4" fontFamily= '"Georgia", "Helvetica", "Arial", sans-serif' color="rgb(66, 86, 66)">Project title</Typography>
             </Paper>
         </Grid>
-    )
+     )
 }
 
 export default ProjectCard;
