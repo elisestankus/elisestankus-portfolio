4b3ab0e3eede1753cffd2e79062e8cf5e0d927c4
estan <elise.stankus@gmail.com>
2024-04-02 13:54:44 -0700

    replaced cc screenshot, made size responsive

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -7,7 +7,7 @@ function ProjectCard() {
     return (
         <Grid item xs={12} sm={6} md={4}>
             <Paper>
-                <img src={ccScreenshot} alt="project screenshot" height="200px" width="200px" /> 
+                <img src={ccScreenshot} alt="project screenshot" height="auto" width="100%" /> 
                 <Typography variant="h4" fontFamily= '"Georgia", "Helvetica", "Arial", sans-serif' color="rgb(66, 86, 66)">Project title</Typography>
             </Paper>
         </Grid>
