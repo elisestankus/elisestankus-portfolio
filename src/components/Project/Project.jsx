4385a23b6062dbfefb5027bd482b458eda1544a0
estan <elise.stankus@gmail.com>
2024-04-02 15:50:26 -0700

    removed project title and associated css

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -7,10 +7,8 @@ import './Project.css'
 function ProjectCard() {
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <Paper className="projectPaper"sx={{backgroundColor: 'whitesmoke', pt: '.1px'}}>
-            <Typography className="projectTitle" variant="h4" fontFamily= '"Georgia", "Helvetica", "Arial", sans-serif' color="rgb(115, 147, 115)" align="center" sx={{mt: '2px', mx: '2px'}}>Crystal Clarity</Typography>
+            <Paper className="projectPaper"sx={{backgroundColor: 'whitesmoke', p: '.1px'}}>
                 <img className="screenshot" src={ccScreenshot} alt="project screenshot" height="auto" width="100%" /> 
-                
             </Paper>
         </Grid>
      )
