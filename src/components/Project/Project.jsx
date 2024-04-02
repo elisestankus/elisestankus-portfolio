139b51511711221f3dd049749fe643663127ffac
estan <elise.stankus@gmail.com>
2024-04-02 15:33:51 -0700

    added border to screenshot and project title, added box-shadow on hover to paper

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -2,13 +2,15 @@ import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import ccScreenshot from '../../assets/ccScreenshot.png';
+import './Project.css'
 
 function ProjectCard() {
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <Paper>
-                <img src={ccScreenshot} alt="project screenshot" height="auto" width="100%" /> 
-                <Typography variant="h4" fontFamily= '"Georgia", "Helvetica", "Arial", sans-serif' color="rgb(66, 86, 66)">Project title</Typography>
+            <Paper className="projectPaper"sx={{backgroundColor: 'whitesmoke', pt: '.1px'}}>
+            <Typography className="projectTitle" variant="h4" fontFamily= '"Georgia", "Helvetica", "Arial", sans-serif' color="rgb(115, 147, 115)" align="center" sx={{mt: '2px', mx: '2px'}}>Crystal Clarity</Typography>
+                <img className="screenshot" src={ccScreenshot} alt="project screenshot" height="auto" width="100%" /> 
+                
             </Paper>
         </Grid>
      )
