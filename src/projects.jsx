9dd98e056de5bf63bd7988d382c08524c2439262
estan <elise.stankus@gmail.com>
2024-04-04 10:00:09 -0700

    add coder cove details

--- src/projects.jsx
+++ src/projects.jsx
@@ -1,6 +1,7 @@
 import ccScreenshot from './assets/ccScreenshot.png';
 import hgScreenshot from './assets/hgScreenshot.png'
 import mjScreenshot from './assets/mjScreenshot.png'
+import coderCoveScreenshot from './assets/coderCoveScreenshot.png'
 
 const projects = [
     {
@@ -26,6 +27,14 @@ const projects = [
         deployedURL: 'https://sav-the-sag.github.io/movie_project/',
         githubRepo: 'https://github.com/elisestankus/movie_journal.git'
         
+    },
+    {
+        imagePath: coderCoveScreenshot,
+        title: 'Coder Cove',
+        description: 'A full stack blog application independently built using MySQL, Handlebars, Express, and Node.js.',
+        deployedURL: 'https://coder-cove-105ba26a819d.herokuapp.com/',
+        githubRepo: 'https://github.com/elisestankus/coder-cove.git'
+
     }
 
 
