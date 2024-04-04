76aebd054fbb506bf8db1c72167b2b5448de3ad0
estan <elise.stankus@gmail.com>
2024-04-03 17:15:49 -0700

    import and map through projects, edit text

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,16 +1,18 @@
 import Grid from '@mui/material/Grid';
+import projects from '../projects.jsx';
 import ProjectCard from '../components/Project/Project';
 
 
 export default function Work() {
     return (
         <div>
-            <h2>Work Page</h2>
+            <h1>Work</h1>
+            <br />
+            <h4>A few projects I've worked on recently!</h4>
             <br />
             < Grid container spacing={3}>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
-                <ProjectCard></ProjectCard>
+                {projects.map(project => <ProjectCard {...project} key={projects.indexOf(project)} />)}
+                
             </Grid>
         </div>
     );
