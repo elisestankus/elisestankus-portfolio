db04ba4a14e2699b89fbbda50f90f2940fa5f80e
estan <elise.stankus@gmail.com>
2024-04-03 17:16:19 -0700

    create projects array, add cc, hg, and mj project details

---
+++ src/projects.jsx
@@ -0,0 +1,34 @@
+import ccScreenshot from './assets/ccScreenshot.png';
+import hgScreenshot from './assets/hgScreenshot.png'
+import mjScreenshot from './assets/mjScreenshot.png'
+
+const projects = [
+    {
+        imagePath: ccScreenshot,
+        title: 'Crystal Clarity',
+        description: 'A personal wellness application collaboratively developed using the MERN stack.',
+        deployedURL: 'https://crystalclarity.onrender.com/',
+        githubRepo: 'https://github.com/elisestankus/crystalclarity.git'
+
+    },
+    {
+        imagePath: hgScreenshot,
+        title: 'HackerGrove',
+        description: 'A habit-tracking application for coders collaboratively built using MySQL, Handlebars, Express, and Node.js. ',
+        deployedURL: 'https://hacker-grove-215ce88624e3.herokuapp.com/',
+        githubRepo: 'https://github.com/elisestankus/hacker-grove.git'
+
+    },
+    {
+        imagePath: mjScreenshot,
+        title: 'Movie Journal',
+        description: 'A habit-tracking application for coders collaboratively built using MySQL, Handlebars, Express, and Node.js. ',
+        deployedURL: 'https://hacker-grove-215ce88624e3.herokuapp.com/',
+        githubRepo: 'https://github.com/elisestankus/hacker-grove.git'
+        
+    }
+
+
+]
+
+export default projects;
