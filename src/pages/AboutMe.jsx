8a2249c93a1e524fdd1ecce968c4b329aa0c962b
estan <elise.stankus@gmail.com>
2024-04-24 12:35:19 -0700

    edit about me text

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -24,7 +24,7 @@ export default function AboutMe() {
                     </Typography>
                     <br />
                     <Typography variant="h6" fontFamily='"Georgia", "Helvetica", "Arial", sans-serif'>
-                    I’ve always been a fast learner, diving deep into acquiring new skills – whether it’s teaching myself to knit, learning a new language, or developing my programming skills. From the UC Berkeley program I now have a solid foundation in the MERN stack, as well as experience with a diverse foundation of front-end and back-end technologies. In addition to the technologies and languages listed below, I am also currently learning AWS and Python.
+                    I’ve always been a fast learner, diving deep into acquiring new skills – whether it’s teaching myself to knit, learning a new language, or developing my programming skills. From the UC Berkeley program I now have a solid foundation in the MERN stack, as well as experience with a diverse collection of front-end and back-end technologies. In addition to the technologies and languages listed below, I am also currently learning AWS and Python.
                     </Typography>
                     <br />
                     <Typography variant="h6" fontFamily='"Georgia", "Helvetica", "Arial", sans-serif'>
